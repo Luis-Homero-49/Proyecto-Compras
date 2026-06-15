807b164bd5876bbf9f0e48ac28f6c02b4ddd77cf
Luis-Homero-49 <luishomerobo@gmail.com>
2026-06-14 20:10:20 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -731,7 +731,7 @@ app.get('/api/price_history/suggest', async (req, res) => {
 // --- CONFIGURACION DE PRODUCCION (RAILWAY) ---
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('*', (req, res) => {
+  app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
   });
 }
